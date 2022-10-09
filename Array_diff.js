28f00a72e9df47dd3c1a6f413f295d24bef1e74c
Nikita Gromov <105276303+Gromov812@users.noreply.github.com>
2022-10-09 10:29:14 +0500

    Create Array_diff.js

---
+++ Array_diff.js
@@ -0,0 +1,14 @@
+    function arrayDiff(a, b) {
+
+for (let el in b) {
+ for (let elem in a) {
+  b[el] == a[elem] ? a[elem] = "toDel": 0;
+ }
+}
+    a = a.join(" ").replace(/[a-zA-Z]/gi, "").split(" ");
+    let rArr = [];
+    for (let el in a){
+        a[el].length > 0 ? rArr.push(a[el]) : 0;
+    }
+return rArr.map(Number);
+}
